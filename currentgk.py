62dd8c800386176962432d6b458f91525365b4bd
ym555 <mohamed.yoyo100@gmail.com>
2023-01-07 07:39:53 +0200

    this time for sure

--- currentgk.py
+++ currentgk.py
@@ -1,6 +1,4 @@
 from itertools import combinations
-#from functools import cache
-
 from pprint import pprint
 from re import search
 from dataclasses import dataclass
@@ -34,7 +32,7 @@ class QuizSetting:
 
     # TODO(ym): deck ranges
     decks: list
-    #decks: frozenset([str])
+    deck_range: tuple
     score_limit: int
 
     max_missed: int
@@ -42,15 +40,17 @@ class QuizSetting:
 
     def similar(self, other):
         errors = []
-        if self.foreground != other.foreground or self.effect != other.effect or set(self.decks) != set(other.decks) or self.shuffle != other.shuffle:
+        if (self.foreground is not None and self.foreground != other.foreground) or (self.effect is not None and self.effect != other.effect) or set(self.decks) != set(other.decks) or self.shuffle != other.shuffle:
             errors.append('Quiz settings are different')
+        if other.deck_range != self.deck_range:
+            errors.append('Deck ranges are different')
         if other.time_limit > self.time_limit or other.additional_answer_time_limit > self.additional_answer_time_limit:
             errors.append("Answer time too long.")
         if other.font_size > self.font_size:
             errors.append("Font size too big.")
         if other.score_limit < self.score_limit:
             errors.append("Score limit too low.")
-        if other.font != self.font:
+        if self.font is not None and other.font != self.font:
             errors.append("Font doesn't match.")
         return errors
 
@@ -58,7 +58,13 @@ class QuizSetting:
     @classmethod
     def from_dict(cls, js):
         s = js['settings']
-        return cls(decks=frozenset([i['shortName'] for i in js['decks']]),
+        deck_range = None
+        for i in js['decks']:
+            if 'startIndex' in i and 'endIndex' in i:
+                deck_range = (i['startIndex'], i['endIndex'])
+                break
+        return cls(decks=[i['shortName'] for i in js['decks']],
+                   deck_range=deck_range,
                    font=s['font'],
                    font_size=s['fontSize'],
                    foreground=s['fontColor'],
@@ -70,24 +76,23 @@ class QuizSetting:
                    shuffle=s['shuffle'] if 'shuffle' in s else s['serverSettings']['shuffle'])
 
     # I don't like this
-    #@cache
     def to_command(self):
         if list(self.decks)[0].startswith("gn"):
-            return f"k!quiz {'+'.join((self.decks))} nd {self.score_limit} mmq={self.max_missed}"
-        return f"k!quiz {'+'.join((self.decks))} {self.score_limit} hardcore nd mmq={self.max_missed} dauq=1 font=5 color={self.foreground} size={self.font_size}" + (f" effect={self.effect}" if self.effect != '' else '')
+            return f"k!quiz {'+'.join(self.decks)} nd {self.score_limit} mmq={self.max_missed}"
+        return f"k!quiz {'+'.join(self.decks)}" + (f"({self.deck_range[0]}-{self.deck_range[1]}) " if self.deck_range else " ") + f"{self.score_limit} hardcore nd mmq={self.max_missed} dauq=1 font=5 color={self.foreground} size={self.font_size}" + (f" effect={self.effect}" if self.effect is not None else '')
 
 RankStructure = {
-    'Student': QuizSetting(font='Eishiikaisho', font_size=100, foreground='#f173ff', effect= '', time_limit=16000, additional_answer_time_limit=0, decks=['jpdb1k'], score_limit=25, max_missed=10, shuffle=True),
-    'Trainee': QuizSetting(font='Eishiikaisho', font_size=100, foreground='#f173ff', effect='antiocr', time_limit=16000, additional_answer_time_limit=0, decks=['jpdb1k'], score_limit=50, max_missed=10, shuffle=True),
-    'Debut Idol': QuizSetting(font='Eishiikaisho', font_size=100, foreground='#f173ff', effect='antiocr', time_limit=16000, additional_answer_time_limit=0, decks=['jpdb2_5k', 'jpdb5k'], score_limit=50, max_missed=10, shuffle=True),
-    'Major Idol': QuizSetting(font='Eishiikaisho', font_size=100, foreground='#f173ff', effect='antiocr', time_limit=16000, additional_answer_time_limit=0, decks=['jpdb5k', 'jpdb10k'], score_limit=50, max_missed=10, shuffle=True),
-    'passed Prima vocab': QuizSetting(font='Eishiikaisho', font_size=100, foreground='#f173ff', effect='antiocr', time_limit=16000, additional_answer_time_limit=0, decks=['jpdb10k', 'jpdb15k'], score_limit=50, max_missed=10, shuffle=True),
-    'passed Divine vocab': QuizSetting(font='Eishiikaisho', font_size=100, foreground='#f173ff', effect='antiocr', time_limit=16000, additional_answer_time_limit=0, decks=['jpdb15k', 'jpdb20k'], score_limit=50, max_missed=10, shuffle=True),
-    'passed Eternal vocab': QuizSetting(font='Eishiikaisho', font_size=100, foreground='#f173ff', effect='antiocr', time_limit=16000, additional_answer_time_limit=0, decks=['jpdb20k', 'jpdb25k'], score_limit=50, max_missed=10, shuffle=True),
-    'GN2': QuizSetting(font='Eishiikaisho', font_size=200, foreground='#f173ff', effect='antiocr', time_limit=16000, additional_answer_time_limit=0, decks=['gn2'], score_limit=20, max_missed=4, shuffle=True),
-    'GN1': QuizSetting(font='Eishiikaisho', font_size=200, foreground='#f173ff', effect='antiocr', time_limit=16000, additional_answer_time_limit=0, decks=['gn1'], score_limit=20, max_missed=4, shuffle=True),
+    'Student': QuizSetting(font='Eishiikaisho', font_size=100, foreground='#f173ff', effect=None, time_limit=16000, additional_answer_time_limit=0, decks=['jpdb1k'], deck_range=(1, 300), score_limit=25, max_missed=10, shuffle=True),
+    'Trainee': QuizSetting(font='Eishiikaisho', font_size=100, foreground='#f173ff', effect='antiocr', time_limit=16000, additional_answer_time_limit=0, decks=['jpdb1k'], deck_range=None, score_limit=50, max_missed=10, shuffle=True),
+    'Debut Idol': QuizSetting(font='Eishiikaisho', font_size=100, foreground='#f173ff', effect='antiocr', time_limit=16000, additional_answer_time_limit=0, decks=['jpdb2_5k', 'jpdb5k'], deck_range=None, score_limit=50, max_missed=10, shuffle=True),
+    'Major Idol': QuizSetting(font='Eishiikaisho', font_size=100, foreground='#f173ff', effect='antiocr', time_limit=16000, additional_answer_time_limit=0, decks=['jpdb5k', 'jpdb10k'], deck_range=None, score_limit=50, max_missed=10, shuffle=True),
+    'passed Prima vocab': QuizSetting(font='Eishiikaisho', font_size=100, foreground='#f173ff', effect='antiocr', time_limit=16000, additional_answer_time_limit=0, decks=['jpdb10k', 'jpdb15k'], deck_range=None, score_limit=50, max_missed=10, shuffle=True),
+    'passed Divine vocab': QuizSetting(font='Eishiikaisho', font_size=100, foreground='#f173ff', effect='antiocr', time_limit=16000, additional_answer_time_limit=0, decks=['jpdb15k', 'jpdb20k'], deck_range=None, score_limit=50, max_missed=10, shuffle=True),
+    'passed Eternal vocab': QuizSetting(font='Eishiikaisho', font_size=100, foreground='#f173ff', effect='antiocr', time_limit=16000, additional_answer_time_limit=0, decks=['jpdb20k', 'jpdb25k'], deck_range=None, score_limit=50, max_missed=10, shuffle=True),
+    'GN2': QuizSetting(font='Eishiikaisho', font_size=200, foreground=None, effect=None, time_limit=16000, additional_answer_time_limit=0, decks=['gn2'], deck_range=None, score_limit=20, max_missed=4, shuffle=True),
+    'GN1': QuizSetting(font='Eishiikaisho', font_size=200, foreground=None, effect=None, time_limit=16000, additional_answer_time_limit=0, decks=['gn1'], deck_range=None, score_limit=20, max_missed=4, shuffle=True),
 }
-QuizCommands = [i.to_command() for i in RankStructure.values()] + ['k!quiz jpdb1k(1-300) 25 hardcore nd mmq=10 dauq=1 font=5 color=#f173ff size=100']
+QuizCommands = [i.to_command() for i in RankStructure.values()]
 pprint(QuizCommands, width=100)
 
 DoubleRanks = [
@@ -99,17 +104,19 @@ DoubleRanks = [
 def get_roles(guild):
     roles = [f for k in RANK_NAMES if (f := get(guild.roles, name=k))]
     if len(roles) != len(RANK_NAMES):
-        return []
+        return {}
     return dict(zip(RANK_NAMES, roles))
 
-async def fail(store, quiz, guild, channel, member):
-    if quiz == 'Student':
-        return
-    quizcommand = RankStructure[quiz].to_command()
-    store.new_quiz_attempt(member.id, quizcommand, datetime.now(), "FAILED")
+async def cooldown(store, channel, member, content):
     unixstamp = store.get_unix()
     await channel.send(f"Please attempt again in <t:{unixstamp}:R> at <t:{unixstamp}>. Any attempts until then will not be counted.")
-    await member.send(f'Please attempt again in <t:{unixstamp}:R> at <t:{unixstamp}>. Any attempts on ``{quizcommand}`` until then will not be counted.')
+    await member.send(f'Please attempt again in <t:{unixstamp}:R> at <t:{unixstamp}>. Any attempts on ``{content}`` until then will not be counted.')
+
+async def fail(store, quiz, guild, channel, member):
+    if quiz == 'Student': return
+    quiz_command =  RankStructure[quiz].to_command()
+    store.new_quiz_attempt(member.id, quiz_command, datetime.now(), "FAILED")
+    await cooldown(store, channel, member, quiz_command)
 
 all_decks = {x for _, v in RankStructure.items() for x in v.decks}
 # This is kind of big, maybe we should make it range(1, 4) max?
@@ -123,19 +130,49 @@ class Quiz(commands.Cog):
         self.store = self.bot.store
 
     @commands.Cog.listener()
+    async def on_member_update(self, before, after):
+        guild_roles = get_roles(before.guild)
+        nqr = set(guild_roles.values()) & (set(after.roles) - set(before.roles))
+        if len(nqr) == 0:
+            return
+        elif len(nqr) > 1:
+            print("len(nqr) > 1")
+
+        nqr = list(nqr)[0]
+        last_quiz, created_at, result = self.store.get_last_attempt(before.id)
+        quiz_name = [k for k, v in RankStructure.items() if v.to_command() == last_quiz][0]
+        if (nqr.name not in {k for k, v in DoubleRanks} and nqr.name != quiz_name) or (datetime.now() - created_at) > timedelta(hours=1) or result != "PASSED":  # Sanity check
+            return
+
+        member = before.guild.get_member(before.id)
+        after_roles = set(map(lambda x: x.name, after.roles))
+        for k, v in DoubleRanks:
+            for i in v:
+                # on_member_update will get called again
+                if k not in after_roles and after_roles.issuperset(i):
+                    return await member.add_roles(guild_roles[k])
+
+        if quiz_name.startswith("passed"):
+            _, z1, z2 = quiz_name.split(" ")
+            quiz_name = f"{z1} Idol {z2}"
+        await member.send(f"You passed the {quiz_name} quiz! Your role is now updated.")
+
+        if not nqr.name.startswith("passed") and not nqr.name.startswith("GN"):
+            rr = set(before.roles) & set(guild_roles.values()) - {guild_roles['GN1'], guild_roles['GN2']}
+            await member.remove_roles(*rr)
+
+            announcement_channel = get(member.guild.channels, name='一般') or member.guild.get_channel(ANNOUNCEMENT_CHANNEL_ID)
+            await announcement_channel.send(f"{member.mention} has passed the {quiz_name} quiz and is now {nqr.mention}!")
+
+    @commands.Cog.listener()
     async def on_message(self, message):
         if any(message.content.startswith(i) for i in COMB_CACHE):
             if message.content in QuizCommands:
-                logs = self.store.get_attempts(
-                    message.author.id, message.content)
-                if logs[0][0] == 0:
+                if not self.store.get_attempts(message.author.id, message.content):
                     return await message.channel.send("This attempt will be counted!")
-                unixstamp = self.store.get_unix()
-                await message.channel.send(f"Please attempt again in <t:{unixstamp}:R> at <t:{unixstamp}>. Any attempts until then will not be counted.")
-                await message.guild.get_member(message.author.id).send(f'Please attempt again in <t:{unixstamp}:R> at <t:{unixstamp}>. Any attempts on ``{message.content}`` until then will not be counted.')
+                await cooldown(self.store, message.channel, message.author, message.content)
                 return await message.author.timeout(utcnow() + timedelta(minutes=2), reason="Invalid quiz attempt")
 
-            await fail(self.store, "Trainee", message.guild, message.channel, message.author)
             await message.channel.send("Wrong quiz command")
             return await message.author.timeout(utcnow() + timedelta(minutes=5), reason="Wrong quiz command")
 
@@ -172,62 +209,20 @@ class Quiz(commands.Cog):
             await message.channel.send('\n'.join(similarity[0][1]))
             return await fail(self.store, similarity[0][0], message.guild, message.channel, member)
 
-        # Sort based on the distance to score_limit, hack to fix student getting assigned instead of trainee
-        similarity = *sorted([i for i in similarity if len(i[1]) == 0],
-                             key=lambda x: -RankStructure[x[0]].score_limit),
-        quiz_name = similarity[0][0]
+        quiz_name = list(sorted([i for i in similarity if len(
+            i[1]) == 0], key=lambda x: -RankStructure[x[0]].score_limit))[0][0]
 
         if len(report['participants']) > 1:
             await message.channel.send('Too many participants.')
-            # This poor guy
             return await fail(self.store, quiz_name, message.guild, message.channel, member)
 
-        before = frozenset(member.roles)
-        print(before)
-
-        if report['scores'][0]['score'] == RankStructure[quiz_name].score_limit:
-            await member.add_roles(guild_roles[quiz_name])
-        else:
+        if report['scores'][0]['score'] != RankStructure[quiz_name].score_limit:
             await message.channel.send("Score and limit don't match.")
             return await fail(self.store, quiz_name, message.guild, message.channel, member)
 
-        async def add_double_ranks(member):
-            member_roles = set(map(lambda x: x.name, member.roles))
-            for k, v in DoubleRanks:
-                for i in v:
-                    if member_roles.issuperset(i):
-                        for f in (i | {'passed Prima vocab', 'passed Divine vocab', 'passed Eternal vocab'}):
-                            if f == 'GN1' or f == 'GN2':
-                                continue
-                            await member.remove_roles(guild_roles[f])
-                        return await member.add_roles(guild_roles[k])
-        await add_double_ranks(member)
+        self.store.new_quiz_attempt(member.id, RankStructure[quiz_name].to_command(), datetime.now(), "PASSED")
+        await member.add_roles(guild_roles[quiz_name]) # on_member_update takes on from here
 
-        print(before)
-        print(member.roles)
-        new_role = list(frozenset(member.roles) - before)
-        print(new_role)
-
-        # TODO(YM): check why this happens
-        # Maybe busy loop until it gets updated?
-        if len(new_role) == 0:
-            print("len(new_role) == 0???")
-            return
-
-        new_role = new_role[0]
-        self.store.save_role_info(member.id, new_role.id, datetime.now())
-
-        if quiz_name.startswith("passed"):
-            z = quiz_name.split(" ")
-            quiz_name = f"{z[1]} Idol {z[2]}"
-        await message.channel.send(f"You passed the {quiz_name} quiz! Your role is now updated.")
-
-        announcement_channel = get(message.guild.channels, name='general')
-        if not announcement_channel:
-            announcement_channel = message.guild.get_channel(
-                ANNOUNCEMENT_CHANNEL_ID)
-        if not (quiz_name.endswith("vocab") and new_role.name.endswith("vocab")) and not (quiz_name[:2] == new_role.name[:2] == "GN"):
-            await announcement_channel.send(f"{member.mention} has passed the {quiz_name} quiz and is now {new_role.mention}!")
 
 class Bot(commands.Bot):
     async def on_ready(self):
@@ -237,7 +232,7 @@ class Bot(commands.Bot):
         self.store = Store(_DB_NAME)
         await self.add_cog(Quiz(self))
 
+
 if __name__ == '__main__':
     meido = Bot(command_prefix='!', intents=discord.Intents.all())
-    meido.run("TOKEN")
-    #meido.run(os.environ['TOKEN'])
+    meido.run(os.environ['TOKEN'])
